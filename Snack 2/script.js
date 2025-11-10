42b01ec9b9fc23871a890ec08d3e5850245baac5
matteolpera <matteo.lepera02@gmail.com>
2025-11-10 15:31:36 +0100

    snack2 end

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -4,3 +4,37 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+//
+// Creazione di un array di squadre di calcio
+const squads = [
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 }
+];
+
+// console.log(squads);
+
+for (let i = 0; i < squads.length; i++) {
+    // console.log(squadre[i]);
+    const curSquad = squads[i];
+    if (curSquad["puntiFatti"] === 0) {
+        curSquad["puntiFatti"] = getRandomNumber(1, 100).toFixed();
+    }
+    if (curSquad["falliSubiti"] === 0) {
+        curSquad["falliSubiti"] = getRandomNumber(1, 100).toFixed();
+    }
+}
+
+console.log("Tutte le squadre:", squads);
+
+const squadsAndFail = squads.map(({ nome, falliSubiti }) => ({ nome, falliSubiti }));
+
+console.log(squadsAndFail);
+
+
+// Funzione numero random
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
